fcda37e6fa1a3bf55cb898b05771d555ce8abe8f
thong <thongnt0306@gmail.com>
2021-12-13 01:09:36 +0700

    Add

---
+++ db/seeds.rb
@@ -0,0 +1,134 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Product.delete_all
+Product.create(
+	:sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
+Product.create(
+    :sku => '1234',
+    :title => 'San pham 1',
+    :price => 30000,
+    :quantity => 10,
+    :image => 'img1'
+    )
+Product.create(
+    :sku => '4567',
+    :title => 'San pham 2',
+    :price => 35000,
+    :quantity => 20,
+    :image => 'img2'
+    )
